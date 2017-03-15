553ce392370af098e75d6ae20b8f9b31c32d42a4
alejandro <alejandro@zeppelin.no>
2017-03-15 09:37:33 +0100

    add companies to the main RootQury

--- schema/schema.js
+++ schema/schema.js
@@ -9,12 +9,28 @@ const {
 } = graphql;
 
 // This objects tells GraphQL what properties is suppossed to have
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
-    id: {type: GraphQLString},
+    id: {type: GraphQLString },
     firstName: {type: GraphQLString},
-    age: {type: GraphQLInt}
+    age: {type: GraphQLInt},
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(resp => resp.data)
+      }
+    }
   }
 });
 
@@ -25,8 +41,15 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: {id: {type: GraphQLString}},
       resolve(parentValue, args) {
-        console.log(' resolving ');  
         return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString }},
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
           .then(resp => resp.data);
       }
     }
